b1090c44c1001d946c33acfb72c546c609468871
Štefan Šimek <simek@triaxis.sk>
2024-10-31 21:36:26 +0100

    fix(lvgl): disable printf format warnings on lv_ formatting functions

--- targets/all/lv_conf.h
+++ targets/all/lv_conf.h
@@ -18,3 +18,7 @@
 #define LV_USE_STDLIB_MALLOC    LV_STDLIB_CLIB
 #define LV_USE_STDLIB_STRING    LV_STDLIB_CLIB
 #define LV_USE_STDLIB_SPRINTF   LV_STDLIB_CUSTOM
+
+// this is a lie, but it prevents LVGL from attributing the printf-like
+// functions with format-checker that breaks on our custom directives
+#define PYCPARSER
