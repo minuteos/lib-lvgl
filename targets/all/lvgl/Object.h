d9273ccd4b7da0ff18ada115417dff99ad049b3b
Štefan Šimek <simek@triaxis.sk>
2025-04-23 11:45:14 +0200

    feat: make all ObjectWrapper helpers const

--- targets/all/lvgl/Object.h
+++ targets/all/lvgl/Object.h
@@ -42,27 +42,27 @@ public:
     constexpr operator lv_obj_t*() const { return obj; }
     constexpr ObjRef Ref() const { return this; }
 
-    void SetPosition(int32_t x, int32_t y)
+    void SetPosition(int32_t x, int32_t y) const
         { lv_obj_set_pos(obj, x, y); }
-    void SetSize(int32_t width, int32_t height)
+    void SetSize(int32_t width, int32_t height) const
         { lv_obj_set_size(obj, width, height); }
-    void SetVisible(bool visible)
+    void SetVisible(bool visible) const
         { SetOpacity(visible ? LV_OPA_100 : LV_OPA_0); }
 
-    void SetStyle(lv_style_prop_t prop, lv_style_value_t value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT)
+    void SetStyle(lv_style_prop_t prop, lv_style_value_t value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT) const
         { lv_obj_set_local_style_prop(obj, prop, value, selector); }
-    lv_style_value_t GetStyle(lv_style_prop_t prop, lv_part_t part = LV_PART_MAIN)
+    lv_style_value_t GetStyle(lv_style_prop_t prop, lv_part_t part = LV_PART_MAIN) const
         { return lv_obj_get_style_prop(obj, part, prop); }
 
-    ALWAYS_INLINE FLATTEN void SetStyle(lv_style_prop_t prop, int32_t value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT)
+    ALWAYS_INLINE FLATTEN void SetStyle(lv_style_prop_t prop, int32_t value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT) const
         { SetStyle(prop, { .num = value }, selector); }
-    ALWAYS_INLINE FLATTEN void SetStyle(lv_style_prop_t prop, lv_color_t value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT)
+    ALWAYS_INLINE FLATTEN void SetStyle(lv_style_prop_t prop, lv_color_t value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT) const
         { SetStyle(prop, { .color = value }, selector); }
 
 #define LV_STYLE_HELPER(name, style, stype, type, value, ...) \
-    ALWAYS_INLINE FLATTEN void Set ## name(type value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT) \
+    ALWAYS_INLINE FLATTEN void Set ## name(type value, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT) const \
         { __VA_ARGS__; SetStyle(style, { .stype = value }, selector); } \
-    ALWAYS_INLINE FLATTEN type Get ## name(lv_part_t part = LV_PART_MAIN) \
+    ALWAYS_INLINE FLATTEN type Get ## name(lv_part_t part = LV_PART_MAIN) const \
         { return (type)GetStyle(style, part).stype; }
 
     LV_STYLE_HELPER(Font, LV_STYLE_TEXT_FONT, ptr, const lv_font_t*, font)
@@ -73,7 +73,7 @@ public:
 
     LV_STYLE_HELPER(TranslateX, LV_STYLE_TRANSLATE_X, num, int, x)
     LV_STYLE_HELPER(TranslateY, LV_STYLE_TRANSLATE_Y, num, int, y)
-    ALWAYS_INLINE FLATTEN void SetTranslate(int x, int y)
+    ALWAYS_INLINE FLATTEN void SetTranslate(int x, int y) const
          { SetTranslateX(x); SetTranslateY(y); }
 
     LV_STYLE_HELPER(Width, LV_STYLE_WIDTH, num, int, width)
@@ -92,23 +92,23 @@ public:
     LV_STYLE_HELPER(GridRowSpan, LV_STYLE_GRID_CELL_ROW_SPAN, num, int, span)
     LV_STYLE_HELPER(GridRowAlign, LV_STYLE_GRID_CELL_Y_ALIGN, num, lv_grid_align_t, align)
 
-    ALWAYS_INLINE FLATTEN void SetGridPlacement(int column, int row)
+    ALWAYS_INLINE FLATTEN void SetGridPlacement(int column, int row) const
         { SetGridColumn(column); SetGridRow(row); }
-    ALWAYS_INLINE FLATTEN void SetGridSpan(int columnSpan, int rowSpan)
+    ALWAYS_INLINE FLATTEN void SetGridSpan(int columnSpan, int rowSpan) const
         { SetGridColumnSpan(columnSpan); SetGridRowSpan(rowSpan); }
-    ALWAYS_INLINE FLATTEN void SetGridAlign(lv_grid_align_t colAlign, lv_grid_align_t rowAlign)
+    ALWAYS_INLINE FLATTEN void SetGridAlign(lv_grid_align_t colAlign, lv_grid_align_t rowAlign) const
         { SetGridColumnAlign(colAlign); SetGridRowAlign(rowAlign); }
 #endif
 
     LV_STYLE_HELPER(BackgroundColor, LV_STYLE_BG_COLOR, color, lv_color_t, color)
     LV_STYLE_HELPER(BackgroundOpacity, LV_STYLE_BG_OPA, num, uint8_t, opacity)
-    ALWAYS_INLINE FLATTEN void SetBackground(lv_color_t color, uint8_t opacity = 255, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT)
+    ALWAYS_INLINE FLATTEN void SetBackground(lv_color_t color, uint8_t opacity = 255, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT) const
         { SetBackgroundColor(color, selector); SetBackgroundOpacity(opacity, selector); }
 
     LV_STYLE_HELPER(BorderWidth, LV_STYLE_BORDER_WIDTH, num, int, width)
     LV_STYLE_HELPER(BorderColor, LV_STYLE_BORDER_COLOR, color, lv_color_t, color)
     LV_STYLE_HELPER(BorderSide, LV_STYLE_BORDER_SIDE, num, lv_border_side_t, side)
-    ALWAYS_INLINE FLATTEN void SetBorder(int width, lv_color_t color, lv_border_side_t side = LV_BORDER_SIDE_FULL, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT)
+    ALWAYS_INLINE FLATTEN void SetBorder(int width, lv_color_t color, lv_border_side_t side = LV_BORDER_SIDE_FULL, lv_style_selector_t selector = LV_PART_MAIN | LV_STATE_DEFAULT) const
         { SetBorderWidth(width, selector); SetBorderColor(color, selector); SetBorderSide(side, selector); }
 
     LV_STYLE_HELPER(TextColor, LV_STYLE_TEXT_COLOR, color, lv_color_t, color)
@@ -118,30 +118,33 @@ public:
     LV_STYLE_HELPER(PaddingBottom, LV_STYLE_PAD_BOTTOM, num, int, padding)
     LV_STYLE_HELPER(PaddingLeft, LV_STYLE_PAD_LEFT, num, int, padding)
     LV_STYLE_HELPER(PaddingRight, LV_STYLE_PAD_RIGHT, num, int, padding)
-    ALWAYS_INLINE FLATTEN void SetPadding(int vertical, int horizontal)
+    ALWAYS_INLINE FLATTEN void SetPadding(int vertical, int horizontal) const
         { SetPaddingTop(vertical); SetPaddingBottom(vertical); SetPaddingLeft(horizontal); SetPaddingRight(horizontal); }
-    ALWAYS_INLINE FLATTEN void SetPadding(int padding)
+    ALWAYS_INLINE FLATTEN void SetPadding(int padding) const
         { SetPadding(padding, padding); }
 
     LV_STYLE_HELPER(MarginTop, LV_STYLE_MARGIN_TOP, num, int, margin)
     LV_STYLE_HELPER(MarginBottom, LV_STYLE_MARGIN_BOTTOM, num, int, margin)
     LV_STYLE_HELPER(MarginLeft, LV_STYLE_MARGIN_LEFT, num, int, margin)
     LV_STYLE_HELPER(MarginRight, LV_STYLE_MARGIN_RIGHT, num, int, margin)
-    ALWAYS_INLINE FLATTEN void SetMargin(int vertical, int horizontal)
+    ALWAYS_INLINE FLATTEN void SetMargin(int vertical, int horizontal) const
         { SetMarginTop(vertical); SetMarginBottom(vertical); SetMarginLeft(horizontal); SetMarginRight(horizontal); }
-    ALWAYS_INLINE FLATTEN void SetMargin(int margin)
+    ALWAYS_INLINE FLATTEN void SetMargin(int margin) const
         { SetMargin(margin, margin); }
 
     static constexpr lv_color_t ColorWhite = { 255, 255, 255 };
     static constexpr lv_color_t ColorBlack = { 0, 0, 0 };
 
-    void AddToGroup(lv_group_t* group = lv_group_get_default()) { lv_group_add_obj(group, obj); }
-    void AddFlag(lv_obj_flag_t flag) { lv_obj_add_flag(obj, flag); }
-    void AddState(lv_state_t state) { lv_obj_add_state(obj, state); }
-    void RemoveFlag(lv_obj_flag_t flag) { lv_obj_remove_flag(obj, flag); }
-    void RemoveState(lv_state_t state) { lv_obj_remove_state(obj, state); }
+    void AddToGroup(lv_group_t* group = lv_group_get_default()) const { lv_group_add_obj(group, obj); }
+    void AddFlag(lv_obj_flag_t flag) const { lv_obj_add_flag(obj, flag); }
+    void AddState(lv_state_t state) const { lv_obj_add_state(obj, state); }
+    void RemoveFlag(lv_obj_flag_t flag) const { lv_obj_remove_flag(obj, flag); }
+    void RemoveState(lv_state_t state) const { lv_obj_remove_state(obj, state); }
+    bool HasFlag(lv_obj_flag_t flag) const { return (obj->flags & flag) == flag; }
+    bool HasAnyFlag(lv_obj_flag_t flag) const { return obj->flags & flag; }
+    bool HasState(lv_state_t state) const { return obj->state & state; }
 
-    void Focus() { lv_group_focus_obj(obj); }
+    void Focus() const { lv_group_focus_obj(obj); }
 
     TypedSpan<ObjectWrapper> GetChildren() const
     {
@@ -154,9 +157,9 @@ public:
     template<typename T> T& UnsafeCast() const { return *static_cast<T*>((ObjectWrapper*)obj->user_data); }
 
 protected:
-    template<auto Handler> void AddEvent(lv_event_code_t event = __EventThunk<Handler>::DefaultEvent)
+    template<auto Handler> void AddEvent(lv_event_code_t event = __EventThunk<Handler>::DefaultEvent) const
     {
-        lv_obj_add_event_cb(obj, __EventThunk<Handler>::cb, event, this);
+        lv_obj_add_event_cb(obj, __EventThunk<Handler>::cb, event, (void*)this);
     }
 
 private:
